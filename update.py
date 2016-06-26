c70b2d226623a69f9abfe9fd3aa618ae8ae8bccd
Lee-Kevin <lidreamer@foxmail.com>
2016-06-26 21:17:37 +0800

    This is a script that can git pull from seeed-wiki, and then mkdocs serve

--- update.py
+++ update.py
@@ -17,9 +17,11 @@ def main():
         else:
             logging.info("Need to mkdocs serve") 
             try:
-                data = subprocess.Popen(cmd_mkdocs_serve, shell=True, stdout=subprocess.PIPE, cwd=path).stdout.read()
-                data.wait()
-                logging.info(data)
+                child = subprocess.Popen(cmd_mkdocs_serve, shell=True, stdout=subprocess.PIPE, cwd=path)
+                time.sleep(10)
+                child.kill()
+                logging.info("Kill the childprocess")
+                kill_process("mkdocs")
             except Exception,e:
                 logging.warn(e)
     except Exception,e:
@@ -27,9 +29,23 @@ def main():
 def UpdatetoCloud():
     logging.info("upload to s3 cloud")
     pass
+
+def kill_process(name):
+    cmd_temp = "ps aux | grep %s" % name
+    data = subprocess.Popen(cmd_temp,shell=True, stdout=subprocess.PIPE).stdout.read()
+    logging.info(data)
+    colum = data.split()
+    cmd_temp = "kill %d" % int(colum[1])
+    data = subprocess.Popen(cmd_temp,shell=True, stdout=subprocess.PIPE).stdout.read()
+    logging.info(data)
+
+
 if __name__ == "__main__":
     logging.info("The Code is begining")
-    main()
+    kill_process("mkdocs")
+    while(True):
+        main()
+        time.sleep(5)
 
 
 #    retcode = p.wait()
